4813667cbf2f26a31a539c4f29e913792f152acc
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2024-05-15 21:34:55 -0700

    Fix paths.

--- langchain_intro/create_retriever.py
+++ langchain_intro/create_retriever.py
@@ -1,13 +1,19 @@
+from pathlib import Path
+
 import dotenv
-from langchain.document_loaders.csv_loader import CSVLoader
+from langchain_community.document_loaders import CSVLoader
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 
-REVIEWS_CSV_PATH = "data/reviews.csv"
-REVIEWS_CHROMA_PATH = "chroma_data"
+parent = Path(__file__).parents[1]
+REVIEWS_CSV_PATH = parent / "data/reviews.csv"
+REVIEWS_CHROMA_PATH = str(parent / "chroma_data")
 
 dotenv.load_dotenv()
 
+# In practice, if you're embedding a large document, you should use a text
+# splitter. For this example, you can embed each review individually because
+# they're relatively small.
 loader = CSVLoader(file_path=REVIEWS_CSV_PATH, source_column="review")
 reviews = loader.load()
 
